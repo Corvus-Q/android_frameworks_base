64814f5601a0844315581043f018f463a88992d3
Kshitij Gupta <kshitijgm@gmail.com>
2018-01-02 15:16:08 +0530

    Custom Rounded Corner and Padding preferences [1/2]
    RoundedCorners: Use even if resource values are 0
    
    Change-Id: Ib8031f420220cdd19da3ced86c066c2d3282d517
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    Settings: Add Rounded corner strings to Settings.Secure
    
    Change-Id: Ifa158a2138e96dbbed68bae0ff178212b061bc65
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    base: Allow using framework values for rounded corners [1/2]
    
    Change-Id: I246a34f0b52ca595f6bcfd7367631673db25c0cf
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    base: corners: Evaluate framework values correctly [2/2]
    
    ScreenDecorations: corners: Fixes for pie
    
    - If the device has framework value 0dp for corners, it would ideally
      misbehave when we override it with our secure setting.
    - To fix this, restore and modify default hasRoundedCorners logic, add
      SIZE tunable unconditionally, setupDecorations if necessary
      onTuningChanged, and make sure to setVisibility when needed.
    
    Change-Id: I9719ab8e18cda2be4db33fa8d871945b6722e2bd
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    StatusBar: corners: Fix dp evaluation
    
    - The secure setting stores the value in dp, not in native px. Divide
      our px value with density to get true dp value.
    - Also, since dp value is stored as an int, use getDimension instead of
      getDimensionPixelSize to  prevent loss during division and rounding
      off.
    
    Change-Id: I5380de944efa324d7633370ed2eda587566e02a3
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    Change-Id: Ifa158a2138e96dbbed68bae0ff178212b061bc65
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -471,6 +471,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     private final DisplayMetrics mDisplayMetrics = Dependency.get(DisplayMetrics.class);
 
     private PackageMonitor mPackageMonitor;
+    private boolean mSysuiRoundedFwvals;
 
     // XXX: gesture research
     private final GestureRecorder mGestureRec = DEBUG_GESTURES
@@ -1957,6 +1958,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.SYSUI_ROUNDED_FWVALS),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -1977,6 +1981,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS))) {
                 setFpToDismissNotifications();
             }
+            update();
         }
 
     public void update() {
@@ -1988,6 +1993,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         setUseLessBoringHeadsUp();
         setPulseOnNewTracks();
         setFpToDismissNotifications();
+        updateCorners();
         }
     }
 
@@ -3632,6 +3638,35 @@ public class StatusBar extends SystemUI implements DemoMode,
         mScrimController.setExpansionAffectsAlpha(true);
     }
 
+    public boolean isCurrentRoundedSameAsFw() {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        Resources res = null;
+        try {
+            res = mContext.getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+            // If we can't get resources, return true so that updateCorners doesn't attempt to
+            // set corner values
+            return true;
+        }
+
+        // Resource IDs for framework properties
+        int resourceIdRadius = res.getIdentifier("com.android.systemui:dimen/rounded_corner_radius", null, null);
+        int resourceIdPadding = res.getIdentifier("com.android.systemui:dimen/rounded_corner_content_padding", null, null);
+
+        // Values on framework resources
+        int cornerRadiusRes = (int) (res.getDimension(resourceIdRadius) / density);
+        int contentPaddingRes = (int) (res.getDimension(resourceIdPadding) / density);
+
+        // Values in Settings DBs
+        int cornerRadius = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.SYSUI_ROUNDED_SIZE, cornerRadiusRes, UserHandle.USER_CURRENT);
+        int contentPadding = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, contentPaddingRes, UserHandle.USER_CURRENT);
+
+        return (cornerRadiusRes == cornerRadius) && (contentPaddingRes == contentPadding);
+    }
+
     /**
      * Switches theme from light to dark and vice-versa.
      */
@@ -3644,6 +3679,31 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mContext.getThemeResId() != themeResId) {
             mContext.setTheme(themeResId);
             Dependency.get(ConfigurationController.class).notifyThemeChanged();
+        }
+        updateCorners();
+    }
+
+    private void updateCorners() {
+        mSysuiRoundedFwvals = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.SYSUI_ROUNDED_FWVALS, 1,
+                UserHandle.USER_CURRENT) == 1;
+        if (mSysuiRoundedFwvals && !isCurrentRoundedSameAsFw()) {
+            float density = Resources.getSystem().getDisplayMetrics().density;
+            Resources res = null;
+            try {
+                res = mContext.getPackageManager().getResourcesForApplication("com.android.systemui");
+            } catch (NameNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            if (res != null) {
+                int resourceIdRadius = res.getIdentifier("com.android.systemui:dimen/rounded_corner_radius", null, null);
+                Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SYSUI_ROUNDED_SIZE, (int) (res.getDimension(resourceIdRadius) / density), UserHandle.USER_CURRENT);
+                int resourceIdPadding = res.getIdentifier("com.android.systemui:dimen/rounded_corner_content_padding", null, null);
+                Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, (int) (res.getDimension(resourceIdPadding) / density), UserHandle.USER_CURRENT);
+            }
         }
     }
 
