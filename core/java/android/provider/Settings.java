4c8ff2d4492377287a713f0c41ef6a181b43adb7
Ali B <abittin@gmail.com>
2018-03-14 20:06:26 +0300

    base: Ambient Display battery toggle [1/2]
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4949,6 +4949,12 @@ public final class Settings {
                 "double_tap_sleep_lockscreen";
 
         /**
+         * Whether to show battery percent on ambient bottom
+         * @hide
+         */
+        public static final String AMBIENT_BATTERY_PERCENT = "ambient_battery_percent";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
