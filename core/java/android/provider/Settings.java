345e89f9dc4304cb26a3041f27efea6e17e4d352
Nico60 <ndhouailly@gmail.com>
2018-04-11 21:03:36 +0200

    base: QS panel opacity [1/2]
    Change-Id: Ieabb916f065a91be0cacbc88fe0028d0ffc19f56

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5308,6 +5308,12 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * Whether to change the transparency of the qs panel
+         * @hide
+         */
+        public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
