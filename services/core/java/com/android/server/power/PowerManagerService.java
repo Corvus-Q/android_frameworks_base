1bb32c38abe3e9114abe4f1c1398b61f48470b1f
ShevT <evgeny.shishkov@gmail.com>
2019-02-17 14:39:44 +0300

    base: Turn on display if Doze On Charge is Enabled
    Regardless of the setting 'Wake up on charge'
    Otherwise, the screen will not show information when charging is connected/disconnected during sleep.
    
    Change-Id: Ic4a8e88c156ad46e187fc91a0e09abbf55bd2337
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -2016,7 +2016,7 @@ public final class PowerManagerService extends SystemService
     private boolean shouldWakeUpWhenPluggedOrUnpluggedLocked(
             boolean wasPowered, int oldPlugType, boolean dockedOnWirelessCharger) {
         // Don't wake when powered unless configured to do so.
-        if (mWakeUpWhenPluggedOrUnpluggedSetting == 0) {
+        if (mWakeUpWhenPluggedOrUnpluggedSetting == 0  && !mDozeOnChargeEnabled) {
             return false;
         }
 
@@ -2046,10 +2046,8 @@ public final class PowerManagerService extends SystemService
         }
 
         // On Always On Display, SystemUI shows the charging indicator
-        if (mAlwaysOnEnabled && mWakefulness == WAKEFULNESS_DOZING) {
-            if (!mDozeOnChargeEnabled) {
-                return false;
-            }
+        if (mAlwaysOnEnabled && !mDozeOnChargeEnabled && mWakefulness == WAKEFULNESS_DOZING) {
+            return false;
         }
 
         // Otherwise wake up!
