bc09aa56b925f8fbb3555ec8a9809950be579b9e
Giuseppe Maggio <jertlok@protonmail.com>
2019-04-09 22:53:57 +0200

    ThemeTile: Make it persistent after a reboot
    Change-Id: Iad868d041f75c1e3f1c166f70e837d834f8728ce

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5927,6 +5927,14 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * The enabled mode for the theme tile.
+         * - ACCENT
+         * - STYLE
+         * @hide
+         */
+        public static final String THEME_TILE_ENABLED_MODE = "theme_tile_enabled_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
