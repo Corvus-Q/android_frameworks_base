d6330c8af5b0f0d5dab90624735bb0a2f908bf6e
Anushek Prasal <anushekprasal@gmail.com>
2019-05-29 01:26:35 +0530

    base: Use wireless charging animation for wired charging too

--- services/core/java/com/android/server/power/Notifier.java
+++ services/core/java/com/android/server/power/Notifier.java
@@ -649,7 +649,7 @@ public class Notifier {
     /**
      * Called when wired charging has started - to provide user feedback
      */
-    public void onWiredChargingStarted(@UserIdInt int userId) {
+    public void onWiredChargingStarted(@UserIdInt int userId,int batteryLevel) {
         if (DEBUG) {
             Slog.d(TAG, "onWiredChargingStarted");
         }
@@ -658,6 +658,7 @@ public class Notifier {
         Message msg = mHandler.obtainMessage(MSG_WIRED_CHARGING_STARTED);
         msg.setAsynchronous(true);
         msg.arg1 = userId;
+        msg.arg1 = batteryLevel;
         mHandler.sendMessage(msg);
     }
 
@@ -820,8 +821,12 @@ public class Notifier {
         mSuspendBlocker.release();
     }
 
-    private void showWiredChargingStarted(@UserIdInt int userId) {
+    private void showWiredChargingStarted(@UserIdInt int userId,int batteryLevel) {
+        playChargingStartedVibration(userId);
         playChargingStartedFeedback(userId);
+        if (mStatusBarManagerInternal != null) {
+            mStatusBarManagerInternal.showChargingAnimation(batteryLevel);
+        }
         mSuspendBlocker.release();
     }
 
@@ -872,7 +877,7 @@ public class Notifier {
                     lockProfile(msg.arg1);
                     break;
                 case MSG_WIRED_CHARGING_STARTED:
-                    showWiredChargingStarted(msg.arg1);
+                    showWiredChargingStarted(msg.arg1,msg.arg2);
                     break;
             }
         }
