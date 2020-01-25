276e3b4d01961a6d153ab58fc5bd00d925a834b3
spezi77 <spezi7713@gmx.net>
2020-01-25 01:13:52 +0100

    KeyguardIndicationController: Protect updateChargingIndication() from NPE. This can happen on Pixel devices using SysUIGoogle instead of SysUI. In that case LottieAnimationView may be null.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -472,6 +472,7 @@ public class KeyguardIndicationController implements StateListener,
     }
 
     private void updateChargingIndication() {
+        if (mChargingIndicationView == null) return;
         if (mChargingIndication && mPowerPluggedIn) {
             mChargingIndicationView.setVisibility(View.VISIBLE);
             mChargingIndicationView.playAnimation();
