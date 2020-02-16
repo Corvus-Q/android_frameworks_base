36c05932967aaf4e0c1c0e89658d3073579ac549
Anushek Prasal <anushekprasal@gmail.com>
2020-02-17 00:09:26 +0530

    Revert "base: incall vibration options [1/3]"
    This reverts commit 3badc6eeacedc87a9d547138365a90e8ad432078.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4975,22 +4975,6 @@ public final class Settings {
         public static final String QS_TILE_TITLE_VISIBILITY = "qs_tile_title_visibility";
 
         /**
-         * Whether the phone vibrates on call connect
-         * @hide
-         */
-        public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
-         /**
-         * Whether the phone vibrates on call waiting
-         * @hide
-         */
-        public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
-         /**
-         * Whether the phone vibrates on disconnect
-         * @hide
-         */
-        public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
-
-        /**
          * Double tap on lockscreen to sleep
          * @hide
          */
