627b49579c710031251715c406f43cbb1fc2daab
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-29 11:33:53 +0700

    Remove hwkeys toogle for gaming mode

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5712,12 +5712,6 @@ public final class Settings {
         public static final String GAMING_MODE_DUMMY = "gaming_mode_dummy";
 
         /**
-         * Whether to disable hardware keys in gaming mode
-         * @hide
-         */
-        public static final String GAMING_MODE_HW_KEYS_TOGGLE = "gaming_mode_hw_keys_toggle";
-
-        /**
          * Whether to disable heads up gaming mode
          * @hide
          */
