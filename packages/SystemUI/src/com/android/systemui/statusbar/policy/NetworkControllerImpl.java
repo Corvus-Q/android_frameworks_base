c9bbcb3ff63816364bb95f35b913b38a40efd424
Weijie Wang <weijiew@codeaurora.org>
2020-05-18 19:25:54 +0800

    SystemUI: support VoWIFI icons
    Show VoWIFI icon when VoWIFI is available
    Show VoWIFI call icon when voice call is over WIFI
    
    CRs-Fixed: 2689484
    Change-Id: Id5b8ea61d8dbf129f17bd6e9cd0f39b4bc440506

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -1146,6 +1146,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         boolean hspaDataDistinguishable;
         boolean inflateSignalStrengths = false;
         boolean alwaysShowDataRatIcon = false;
+        boolean showVowifiIcon = false;
         boolean showVolteIcon;
         public String patternOfCarrierSpecificDataIcon = "";
         public long nrIconDisplayGracePeriodMs;
@@ -1206,6 +1207,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
                         config);
             }
             config.showVolteIcon = res.getBoolean(R.bool.config_display_volte);
+            config.showVowifiIcon = res.getBoolean(R.bool.config_display_vowifi);
             return config;
         }
 
