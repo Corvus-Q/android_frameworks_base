aa141981f153d3a92dbf0242b3b2e042ee0f95e6
SuperDroidBond <superdroidbond@yahoo.com>
2020-05-21 01:08:08 +0530

    Random Qs Tint: tint active QS Tiles with random colors [1/2]
    Xtended the QS Tint Options to -:
      1) 1 = Random Color Tint (Default)
      2) 2 = Accent Based Tint
      3) 3 = Off
    Corvus: Adapt for our code without qs themes
    
    Special Thanks to Potato ROM for idea -
        https://github.com/PotatoProject/frameworks_base/commit/8b30ef966ddb8f28763998e3aabf69e62b7351a4
    
    Change-Id: I388c68c69bc5da791e617dc87e46c077805a9052

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -28,8 +28,10 @@ import static com.android.internal.logging.nano.MetricsProto.MetricsEvent.TYPE_A
 import static com.android.settingslib.RestrictedLockUtils.EnforcedAdmin;
 
 import android.app.ActivityManager;
+import android.content.res.Configuration;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.metrics.LogMaker;
 import android.os.Handler;
@@ -71,6 +73,7 @@ import com.android.systemui.qs.QuickStatusBarHeader;
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Base quick-settings tile, extend this to create a new tile.
@@ -434,7 +437,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
 
     public static int getColorForState(Context context, int state) {
 
-        boolean setQsUseNewTint = Settings.System.getIntForUser(context.getContentResolver(),
+        int setQsUseNewTint = Settings.System.getIntForUser(context.getContentResolver(),
                     Settings.System.QS_PANEL_BG_USE_NEW_TINT, 0, UserHandle.USER_CURRENT) == 1;
 
         switch (state) {
@@ -444,7 +447,9 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
             case Tile.STATE_INACTIVE:
                 return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary);
             case Tile.STATE_ACTIVE:
-                if (setQsUseNewTint)
+                    if (setQsUseNewTint == 1)
+                        return QSTileBaseView.randomColor(QSTileBaseView.isThemeDark(context));
+                    else if (setQsUseNewTint == 2)
                    return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
                 else
                    return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
