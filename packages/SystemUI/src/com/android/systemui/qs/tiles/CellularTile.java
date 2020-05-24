ae9f981ed4b725bf6918a0423a8b8e0f53747a14
AnkitGourav <Ankitgourav035@gmail.com>
2020-05-24 11:26:38 +0530

    SystemUI: DataUsageDetailView: add ability to show daily data usage
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -371,7 +371,6 @@ public class CellularTile extends QSTileImpl<SignalState> {
             final DataUsageDetailView v = (DataUsageDetailView) (convertView != null
                     ? convertView
                     : LayoutInflater.from(mContext).inflate(R.layout.data_usage, parent, false));
-
             DataUsageController.DataUsageInfo info = null;
             int defaultSubId = SubscriptionManager.getDefaultDataSubscriptionId();
             if (defaultSubId == SubscriptionManager.INVALID_SUBSCRIPTION_ID) {
@@ -380,8 +379,15 @@ public class CellularTile extends QSTileImpl<SignalState> {
                 info = mDataController.getDataUsageInfo(
                         DataUsageUtils.getMobileTemplate(mContext, defaultSubId));
             }
+            DataUsageController.DataUsageInfo info_dialy = null;
+            if (defaultSubId == SubscriptionManager.INVALID_SUBSCRIPTION_ID) {
+                info_dialy = mDataController.getDailyDataUsageInfo();
+            } else {
+                info_dialy = mDataController.getDailyDataUsageInfo(
+                        DataUsageUtils.getMobileTemplate(mContext, defaultSubId));
+            }
             if (info == null) return v;
-            v.bind(info);
+            v.bind(info, info_dialy);
             v.findViewById(R.id.roaming_text).setVisibility(mSignalCallback.mInfo.roaming
                     ? View.VISIBLE : View.INVISIBLE);
             return v;
