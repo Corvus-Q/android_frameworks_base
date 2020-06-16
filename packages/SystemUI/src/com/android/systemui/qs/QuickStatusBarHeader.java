685720deb9b3a92eeee22ee40797d631330027e6
rohan <purohit.rohan@gmail.com>
2020-06-16 04:12:14 -0400

    SystemUI: set proper tint for data usage image
    Change-Id: I0a7e85885300dd8e6ce20e63df40e616efd16354

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -265,6 +265,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mIconManager.setTint(fillColor);
         mNextAlarmIcon.setImageTintList(ColorStateList.valueOf(fillColor));
         mRingerModeIcon.setImageTintList(ColorStateList.valueOf(fillColor));
+        mDataUsageImage.setImageTintList(ColorStateList.valueOf(fillColor));
 
         mBatteryMeterView = findViewById(R.id.battery);
         mBatteryMeterView.setForceShowPercent(true);
